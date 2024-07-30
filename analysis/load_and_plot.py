d1510f94f0df3be0d64cc4a1b344a92c960ef83e
a-del <adelrocq@laposte.net>
2024-07-30 14:21:49 +0200

    Plots legends

--- analysis/load_and_plot.py
+++ analysis/load_and_plot.py
@@ -22,32 +22,43 @@ def get_all_data(path):
             encoder_runs.append({"name": file[:-3], "epochs": data["epoch"], "train_loss": data["train loss"]})
         else:
             eval_runs.append({"name": file[:-3], "encoder_run": os.path.basename(args.output).split("_clf")[0],
-                              "epochs": data["epoch"], "train_loss": data["train loss"], "test_err": data["terr"], "best_acc": data["best"]["acc"] if "best" in data and "acc" in data["best"] else np.nan})
+                              "epochs": data["epoch"], "train_loss": data["train loss"], "test_err": data["terr"],
+                              "best_acc": data["best"]["acc"] if "best" in data and "acc" in data["best"] else np.nan,
+                              "best_ep": data["best"]["epoch"] if "best" in data and "epoch" in data["best"] else np.nan},
+                             )
     return pd.DataFrame(encoder_runs), pd.DataFrame(eval_runs)
 
 
 def load_and_plot_all(path="logs/"):
     encoder_runs, eval_runs = get_all_data(path)
-    plot_all_train_losses(encoder_runs)
-    plot_all_train_losses(eval_runs)
+    plot_all_train_losses(encoder_runs, title="encoder")
+    plot_all_train_losses(eval_runs, title="decoder")
     plot_all_test_errors(eval_runs)
     plt.show()
 
 
-def plot_all_train_losses(df:pd.DataFrame):
+def plot_all_train_losses(df:pd.DataFrame, title=None):
     fig, ax = plt.subplots()
     for _, row in df.iterrows():
         ax.plot(row.epochs, row.train_loss, label=row["name"])
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Train loss")
     ax.legend()
     ax.set_yscale('log')
+    ax.set_title(title)
 
 
-def plot_all_test_errors(df:pd.DataFrame):
+def plot_all_test_errors(df:pd.DataFrame, title=None):
     fig, ax = plt.subplots()
     for _, row in df.iterrows():
-        ax.plot([ep for ep in row.epochs if not ep%10], row.test_err, label=row["name"])
+        test_epochs = [ep for ep in row.epochs if not ep%10]
+        ax.plot(test_epochs, row.test_err, label=row["name"])
+        ax.scatter([df["best_ep"]], [df["best_acc"]], marker="*")
     ax.legend()
-    # ax.set_yscale('log')
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Test error (%)")
+    ax.set_title(title)
+
 
 
 if __name__ == '__main__':
